c2f04a7260a3dfe19527ac05737bbfc0b3e1f1e0
pavel <pasha.antunovich@gmail.com>
2019-11-04 23:47:01 +0300

    lesson 5 canBuildFrom correction

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -257,7 +257,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean {
-    val setOfChar = chars.joinToString(separator = "", transform = { it -> "${it.toLowerCase()}"}).toSet()
+    val setOfChar = chars.joinToString(separator = "", transform = { "${it.toLowerCase()}"}).toSet()
     val wordToBuild = word.toLowerCase().toSet()
     return setOfChar.containsAll(wordToBuild)
 }
